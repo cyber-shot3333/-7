8509a35e1c5c491485a6a30621c87ec98d29c34d
Илья Смирнов <cyber-shot3333@yandex.ru>
2021-11-08 16:12:18 +0300

    Добавьте файлы проекта.

---
+++ Задание 7.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Calc(ref a);
+            Console.ReadKey();
+        }
+                static void Calc(ref int a)
+        {
+            double v = Math.Pow(a, 3);
+            double s = 6 * Math.Pow(a, 2);
+            Console.WriteLine("Объем куба = {0}",v);
+            Console.WriteLine("Площадь поверхности куба = {0}", s);
+        }
+    }
+}
---
+++ Задание 7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны для 1 треугольника");
+            double a1 = Convert.ToInt32(Console.ReadLine());
+            double b1 = Convert.ToInt32(Console.ReadLine());
+            double c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стороны для 2 треугольника");
+            double a2 = Convert.ToInt32(Console.ReadLine());
+            double b2 = Convert.ToInt32(Console.ReadLine());
+            double c2 = Convert.ToInt32(Console.ReadLine());
+            Triangl(ref a1, b1, c1, a2, b2, c2);
+            Console.ReadKey();
+        }
+        static void Triangl(ref double a1, double b1, double c1, double a2, double b2, double c2)
+        {
+            double p1 = (a1 + b1 + c1)/2;
+            double p2 = (a2 + b2 + c2)/ 2;
+            double s1 = Math.Sqrt(p1 * (p1 - a1) * (p1 - b1) * (p1 - c1));
+            double s2 = Math.Sqrt(p2 * (p2 - a2) * (p2 - b2) * (p2 - c2));
+            Console.WriteLine("Площадь 1-го треугольника {0}", s1);
+            Console.WriteLine("Площадь 2-го треугольника {0}", s2);
+            if (s1 < s2)
+            {
+                Console.WriteLine("Макс площадь у 2-го треугольника");
+            }
+            else
+            {
+                Console.WriteLine("Макс площадь у 1-го треугольника");
+            }
+        }
+    }
+}
+
